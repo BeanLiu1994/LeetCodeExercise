b39968d6a607deb0f6aaa4bc6966dfe11095deef
BeanLiu <njueebeanliu@hotmail.com>
2016-01-30 10:23:38 +0800

    add 3sum

--- LeetCodeTest/ThreeSum.cpp
+++ LeetCodeTest/ThreeSum.cpp
@@ -10,21 +10,24 @@ void ThreeSum::Process()
 vector<vector<int>> ThreeSum::threeSum(vector<int>& nums)
 {
 	vector<vector<int>> result;
-	int nums_size = nums.size();
+	int nums_size = nums.size(), sum = 0;
 	if (nums_size < 3)
 		return result;// Empty cases.
 	sort(nums.begin(), nums.end());
 	for (size_t p_s = 0; p_s < nums_size; ++p_s)
 	{
-		if (p_s>=1 && -nums[p_s] == -nums[p_s - 1])continue;//avoid duplicate triplets.
-		if (nums[p_s] + nums[p_s + 1] + nums[p_s + 2] > 0)break;//minimum sum bigger than 0.
-		for (size_t p_e = nums_size - 1; p_e > p_s + 1; --p_e)
+		if (p_s >= 1 && nums[p_s] == nums[p_s - 1])continue;//avoid duplicate triplets.
+		if (nums[p_s] + nums[p_s + 1] + nums[p_s + 2] > sum)break;//minimum sum bigger than sum.
+
+		for (size_t p_e = nums_size - 1, p_m = p_s + 1; p_m < p_e;)
 		{
-			if (p_e<nums_size - 1 && -nums[p_e] == -nums[p_e + 1])continue;//avoid duplicate triplets.
-			if (nums[p_s] + nums[p_e] + nums[p_e - 1] < 0)break;//maximum sum smaller than 0. one of the number is set to nums[p_s]
-			if (binary_search(nums.begin() + p_s + 1, nums.begin() + p_e, -nums[p_s] + -nums[p_e]))
-			{
-				result.push_back(vector<int>{ nums[p_s], -nums[p_s] + -nums[p_e], nums[p_e] });
+			if (nums[p_m] + nums[p_e] > sum - nums[p_s]) p_e--;
+			else if (nums[p_m] + nums[p_e] < sum - nums[p_s]) p_m++;
+			else {
+				result.push_back(vector<int>{nums[p_s], nums[p_m], nums[p_e]});
+				while (p_m < p_e && nums[p_e] == nums[p_e - 1]) p_e--;//avoid duplicate triplets.
+				while (p_m < p_e && nums[p_m] == nums[p_m + 1]) p_m++;
+				p_e--; p_m++;
 			}
 		}
 	}

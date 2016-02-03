eefc434e5464c1d1bd6c84bb0626701a9dc0c370
BeanLiu <njueebeanliu@hotmail.com>
2016-02-03 22:56:28 +0800

    修改完毕

---
+++ LeetCodeTest/test.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "TestSeg.h"
+//测试方式:将代码直接放进来
+class ConCreteTestSeg :public TestSeg
+{
+private:
+#pragma region [Content of test.cpp]
+	//=============================== File Start ===============================
+#include "Solution.h"
+
+	int getPeachNumber(int n)
+	{
+		if (n == 10)
+		{
+			return 1;
+		}
+		else
+		{
+			int num = (getPeachNumber(n + 1) + 1) * 2;
+			cout << "第" << n << "天所剩桃子" << num << "个" << endl;
+			return num;
+		}
+	}
+	int main()
+	{
+		int num = getPeachNumber(1);
+		cout << "猴子第一天摘了:" << num << "个桃子。" << endl;
+		return 0;
+	}
+	//================================ File End ================================
+#pragma endregion
+};
+//另一种测试方式:直接include文件,但是对应文件需要设置为"从生成中排除"
+class ConCreteTestSeg2 :public TestSeg
+{
+private:
+#pragma region [Content of test.cpp]
+	//=============================== File Start ===============================
+#include "test.cpp"
+	//================================ File End ================================
+#pragma endregion
+};
